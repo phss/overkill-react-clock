55b8d8eb22df367bb80fcb2231f3590f272065d4
Paulo Schneider <paulo.schneider@gmail.com>
2018-02-11 11:09:09 +0000

    Convert element into a functional component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-    const element = (
+function Clock(props) {
+    return (
         <div>
             <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <h2>It is {props.time.toLocaleTimeString()}.</h2>
         </div>
     );
-    ReactDOM.render(element, document.getElementById('root'));
+}
+
+function tick() {
+    ReactDOM.render(
+        <Clock time={new Date()}/>,
+        document.getElementById('root'));
 }
 
 setInterval(tick, 1000);
