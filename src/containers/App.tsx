6647e52a819a2e94086be2ee1f994a76d74589fc
Paulo Schneider <paulo.schneider@gmail.com>
2020-04-12 15:49:50 +0100

    Add clock icons to title

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 import { TimeState } from '../reducers/timeKeeper'
 import { Clock, PartOfDay } from '../components'
 import { TimeProps } from '../components/types'
 import { ClosablePanel } from '../components/ClosablePanel'
+import './App.css'
 
-const AppComponent = (props: TimeProps) => {
-  return (
-    <div>
-      <h1>Overkill React Clock</h1>
-      <Clock {...props} />
-      <PartOfDay {...props} />
-      <ClosablePanel title="Configuration" open={false}>
-        <p>Hello there</p>
-      </ClosablePanel>
-    </div>
-  )
-}
+const Title = () => (
+  <h1 className="title">
+    <FontAwesomeIcon icon={faClock} />
+    Overkill React Clock
+    <FontAwesomeIcon icon={faClock} />
+  </h1>
+)
+
+const AppComponent = (props: TimeProps) => (
+  <div>
+    <Title />
+    <Clock {...props} />
+    <PartOfDay {...props} />
+    <ClosablePanel title="Configuration" open={false}>
+      <p>Hello there</p>
+    </ClosablePanel>
+  </div>
+)
 
 const mapStateToProps = ({ time }: TimeState): TimeProps => ({ time })
 
