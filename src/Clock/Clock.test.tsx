d215d50c9257fd9e8a6d50872df8122f531975c9
Paulo Schneider <paulo.schneider@gmail.com>
2020-04-18 12:23:59 +0100

    Add test for rendering clock with initial state

--- src/Clock/Clock.test.tsx
+++ src/Clock/Clock.test.tsx
@@ -6,22 +6,34 @@ import { Clock } from './Clock'
 import reducer from './reducers'
 
 describe('Clock component', () => {
-  const testStore = createStore(reducer)
-
-  it('updates on UPDATE_CLOCK action', () => {
+  it('renders clock', () => {
+    const date = new Date(2018, 2, 11, 11, 49, 13)
+    const testStore = createStore(reducer, { time: date })
     const { getByText } = render(
       <Provider store={testStore}>
         <Clock />
       </Provider>
     )
-    const date = new Date(2018, 2, 11, 16, 30, 7)
+
+    expect(getByText(date.toLocaleTimeString())).toBeInTheDocument()
+    expect(getByText('It is morning')).toBeInTheDocument()
+  })
+
+  it('renders on UPDATE_CLOCK action', () => {
+    const testStore = createStore(reducer)
+    const { getByText } = render(
+      <Provider store={testStore}>
+        <Clock />
+      </Provider>
+    )
+    const newDate = new Date(2018, 2, 11, 16, 30, 7)
 
     testStore.dispatch({
       type: 'UPDATE_CLOCK',
-      time: date
+      time: newDate
     })
 
-    expect(getByText(date.toLocaleTimeString())).toBeInTheDocument()
+    expect(getByText(newDate.toLocaleTimeString())).toBeInTheDocument()
     expect(getByText('It is afternoon')).toBeInTheDocument()
   })
 })
