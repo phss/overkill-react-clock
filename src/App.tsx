abe3f5d77f29ef861f0f9b64d16d479e3536e91a
Paulo Schneider <paulo.schneider@gmail.com>
2020-05-02 15:22:17 +0100

    Replace title component by mui Typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Container } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles'
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import './App.css'
 import { Clock } from './Clock'
 import clockReducer from './Clock/reducers'
 import clockSaga from './Clock/sagas'
 import { Configuration } from './Configuration'
 import configurationReducer from './Configuration/reducers'
+import { theme } from './theme'
+import { Title } from './styles'
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
@@ -24,11 +26,9 @@ const store = createStore(
 )
 sagaMiddleware.run(clockSaga)
 
-const Title = () => <h1 className="title">Overkill React Clock</h1>
-
 const AppComponent = () => (
   <Container>
-    <Title />
+    <Title variant="h1">Overkill React Clock</Title>
     <Clock />
     <Configuration />
   </Container>
@@ -36,6 +36,8 @@ const AppComponent = () => (
 
 export const App = () => (
   <Provider store={store}>
-    <AppComponent />
+    <ThemeProvider theme={theme}>
+      <AppComponent />
+    </ThemeProvider>
   </Provider>
 )
---
+++ src/styles.ts
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core'
+import { styled } from '@material-ui/core/styles'
+
+export const Title = styled(Typography)({
+  fontWeight: 'lighter',
+  letterSpacing: '10px',
+  fontSize: '64px',
+  margin: '42px'
+  // '@media(max-width: 920px)': {
+  //   color: 'red',
+  //   fontSize: '42px'
+  // }
+})
