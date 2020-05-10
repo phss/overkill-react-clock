684003ee3e7c5b3c36a76c36fa6ecc5e52d650c5
Paulo Schneider <paulo.schneider@gmail.com>
2020-05-10 17:54:55 +0100

    Change palette to dark

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,8 @@ import { createMuiTheme } from '@material-ui/core/styles'
 export const theme = createMuiTheme({
   typography: {
     fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+  },
+  palette: {
+    type: 'dark'
   }
 })
